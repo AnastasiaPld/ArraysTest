a9bd6ba209d4098741427dc199caa741f01094de
adzhevizova <nastq_91_pld@abv.bg>
2021-09-14 17:54:34 +0300

    changes

--- src/main/java/Jsoon.java
+++ src/main/java/Jsoon.java
@@ -1,9 +1,13 @@
+import net.minidev.json.parser.JSONParser;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Jsoon {
     public static void main(String[] args) throws IOException {
@@ -12,12 +16,21 @@ public class Jsoon {
 
         reader = new BufferedReader(new FileReader(
                 "/Users/adzhevizova/Desktop/Json.txt"));
+        ArrayList<String[]> listOfLines = new ArrayList<>();
 
         while ((line = reader.readLine()) != null) {
+            String[] newLine = line.split("");
+            listOfLines.add(newLine);
 
-            System.out.println(line);
+        }
+        for (String[] element : listOfLines) {
+            for (String value : element) {
+                System.out.println(value);
+            }
+
         }
 
     }
 }
+
 
